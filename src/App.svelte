0bb1b3d10dcc6ecb8b22f50fe8ed9b71576cf4fd
Cornel <filipcornelcristian@gmail.com>
2020-05-07 14:41:30 +0300

    add loading indicator and send the correct data

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,29 @@
 <script>
   let hobbies = [];
   let hobbieInput;
+  let isLoading = false;
 
   const addHobbie = () => {
     // console.log({ hobbieInput });
     hobbies = [...hobbies, hobbieInput.value];
 
+    isLoading = true;
     fetch("https://svelte-rest-api.firebaseio.com/hobbies.json", {
       method: "POST",
-      body: JSON.stringify(hobbies),
+      body: JSON.stringify(hobbieInput.value),
       headers: {
         "Content-Type": "application/json"
       }
     })
       .then(result => {
+        isLoading = false;
         if (!result.ok) {
           throw new Error("Failed!");
         }
+        alert("Saved data!");
       })
       .catch(error => {
+        isLoading = false;
         console.log(`Error: ${error}`);
       });
   };
@@ -28,8 +33,12 @@
 <input type="text" id="hobbie" bind:this={hobbieInput} />
 <button on:click={addHobbie}>Add Hobbie</button>
 
-<ul>
-  {#each hobbies as hobby}
-    <li>{hobby}</li>
-  {/each}
-</ul>
+{#if isLoading}
+  <p>Loading...</p>
+{:else}
+  <ul>
+    {#each hobbies as hobby}
+      <li>{hobby}</li>
+    {/each}
+  </ul>
+{/if}
