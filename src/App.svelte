901827e46f9fec299e0c581e4ceb2c4a6f666e93
Cornel <filipcornelcristian@gmail.com>
2020-05-07 14:16:25 +0300

    send data via POST request

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,35 @@
 <script>
+  let hobbies = [];
+  let hobbieInput;
 
+  const addHobbie = () => {
+    // console.log({ hobbieInput });
+    hobbies = [...hobbies, hobbieInput.value];
+
+    fetch("https://svelte-rest-api.firebaseio.com/hobbies.json", {
+      method: "POST",
+      body: JSON.stringify(hobbies),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(result => {
+        if (!result.ok) {
+          throw new Error("Failed!");
+        }
+      })
+      .catch(error => {
+        console.log(`Error: ${error}`);
+      });
+  };
 </script>
 
+<label for="hobbie">Hobbie</label>
+<input type="text" id="hobbie" bind:this={hobbieInput} />
+<button on:click={addHobbie}>Add Hobbie</button>
 
+<ul>
+  {#each hobbies as hobby}
+    <li>{hobby}</li>
+  {/each}
+</ul>
